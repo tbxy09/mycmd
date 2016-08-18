70f371ae7543722c54ee576b60f31f600bbd80b5
tbxy09 <tb_xy09@163.com>
2016-08-18 10:50:42 +0800

    delete files

--- helpdic.js
+++
@@ -1,6 +0,0 @@
-conn = new Mongo("192.168.1.102:27017");
-db = conn.getDB("Tb_xy09");
-var docp=db.helpdict.find({"kw":"%~dp0"})
-doc2 = { "_id" : ObjectId("57a304f9d646603a0491e2b3"), "kw" : [ "%~dp0", "%cd%" ], "ref" : "httppath" }
-var doc = docp.hasNext()?docp.next():null
-printjson(doc.ref)
