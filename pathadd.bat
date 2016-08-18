c5bd1a39a090922c92c9ef6de1454accd7a224c8
tbxy09 <tb_xy09@163.com>
2016-08-18 10:42:07 +0800

    add some macro to pathadd.bat

--- pathadd.bat
+++ pathadd.bat
@@ -1,1 +1,7 @@
 for /f "delims=" %%a in (pathlist.txt) do set PATH=%%a;%PATH%
+
+echo add remotemongo "192.168.1.102:27017"
+set remotemongo=192.168.1.102:27017
+echo add escd as \"%cd%\"
+set escd=\"%cd%\"
+
