70683b09c6d11e9261de5c962ead2eac6e8bbca2
tbxy09 <tb_xy09@163.com>
2016-08-26 11:44:22 +0800

    update pathadd.bat prove the echo off statement doesn’t work , look back later

--- pathadd.bat
+++ pathadd.bat
@@ -1,7 +1,9 @@
+rem @echo off
+rem setlocal enabledelayedexpansion
+
 for /f "delims=" %%a in (pathlist.txt) do set PATH=%%a;%PATH%
 
 echo add remotemongo "192.168.1.102:27017"
 set remotemongo=192.168.1.102:27017
 echo add escd as \"%cd%\"
 set escd=\"%cd%\"
-
