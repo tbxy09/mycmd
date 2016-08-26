add739bc378d11f973fbff9745ee53644849b61a
tbxy09 <tb_xy09@163.com>
2016-08-26 11:38:11 +0800

    update savetomongo.bat for issue, embeded mongoimport into if statement, it will have problems.

--- savetomongo.bat
+++ savetomongo.bat
@@ -16,8 +16,17 @@ goto end
 :main
 echo main
 if "%3"=="" (
-	mongoimport --host 192.168.1.102 --port 27017 --db Tb_xy09 --collection %2 --file %1
-	) else goto fork
-:fork
+     goto fork1
+)else goto fork2
+
+:fork1
+mongoimport --host 192.168.1.102 --port 27017 --db "Tb_xy09" --collection %2 --file "%1"
+goto output
+
+:fork2
 mongoimport --host 192.168.1.102 --port 27017 --db %3 --collection %2 --file %1
+goto output
+
+output
+
 :end
